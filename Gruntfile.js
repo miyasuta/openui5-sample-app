2db17b4ea0578ec3968a01261237e26df5f72319
Mio Yasutake <mio.fujita.01@gmail.com>
2020-05-15 19:41:56 +0900

    Modify Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
     "use strict"
-    grunt.loadNPMTasks("@sap/grunt-sapui5-bestpractice-build");
+    grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
     grunt.config.merge({ compatVersion: "1.77" });
     grunt.registerTask("buildProject", [
         "clean",
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         "build"
     ]);
 
-    grunt.loadNPMTasks("@sap/grunt-sapui5-bestpractice-test");
+    grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
     grunt.registerTask("testProject", ["test"]);
     grunt.config.merge({
         coverage_threshold: {
