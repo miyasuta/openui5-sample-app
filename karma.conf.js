cd92760c418a9d8e38dfaa2b00405e95bb2a0079
Mio Yasutake <mio.fujita.01@gmail.com>
2020-05-19 19:36:19 +0900

    modify console message

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function(config) {
 	"use strict";
 
-	console.log(process.env.CHROME_BIN);
+	console.log("CHROME_BIN: " + process.env.CHROME_BIN);
 
 	var chromeFlags = [
 		"--window-size=1280,1024"
