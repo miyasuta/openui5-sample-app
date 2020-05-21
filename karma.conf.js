2d556c34a451be0db0e09dc4ea0e7385f9810964
Mio Yasutake <mio.fujita.01@gmail.com>
2020-05-21 20:06:59 +0900

    add user-agent

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
 			},
 			CustomChromeHeadless: {
 				base: "ChromeHeadless",
-				flags: chromeFlags
+				flags: ['--no-sandbox', "f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'"]
 			}
 		},
 
